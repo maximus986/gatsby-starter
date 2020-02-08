f141e8bc4ccadb696c630850dec8d76a58d9f546
Aleksandar Maksimovic <aleksandar.maksimovic@p3ds.net>
2020-02-08 13:15:36 +0100

    Add global styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,28 @@
-import React from "react"
+import React from 'react';
+import { Global, css } from '@emotion/core';
 
-const Layout = ({ children }) => <div>{children}</div>
+const Layout = ({ children }) => (
+  <>
+    <Global
+      styles={css`
+        * {
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+        }
+        html {
+          font-size: 10px;
+        }
+        body,
+        html,
+        #___gatsby,
+        #___gatsby > div {
+          height: 100%;
+        }
+      `}
+    />
+    {children}
+  </>
+);
 
-export default Layout
+export default Layout;
