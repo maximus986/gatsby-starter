9dd002b06ef6015ecf9a814ba07a09ea9598b1bd
Aleksandar Maksimovic <aleksandar.maksimovic@p3ds.net>
2020-02-08 12:40:59 +0100

    Starter clean-up

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Starter`,
+    description: `Gatsby Starter`,
+    author: `@AleksandarM986`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
